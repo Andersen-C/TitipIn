1a4df00e56e85ac76686bc759551d92b36093bb1
unknown <kevinaprilian128@gmail.com>
2025-12-10 02:11:37 +0700

    Perubahan di HomeController(kevin)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,12 +13,32 @@ class HomeController extends Controller
     public function adminHome() {
         return view('admin.adminHome');
     }
+        
+    public function titiperHome()
+{
+    // Ambil pesanan terbaru user yg login (titiper/pembeli)
+    $latestOrders = \App\Models\Order::with(['menu', 'user'])
+        ->where('titiper_id', auth()->id())
+        ->latest()
+        ->take(4)
+        ->get();
 
-    public function titiperHome() {
-        return view('titiper.titiperHome');
-    }
+    // Menu terlaris / fallback ke menu terbaru
+    
+
+    // Rekomendasi menu (6 item)
+    $recommended = \App\Models\Menu::latest()->take(6)->get();
+
+    return view('titiper.titiperHome', compact(
+        'latestOrders',
+        'recommended'
+    ));
+}
+
 
     public function runnerHome() {
         return view('runner.runnerHome');
     }
+
+    
 }
